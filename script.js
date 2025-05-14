db98d0b271e203ce1324eb7edd06d8efe081ae7d
Charos <rozamatyoqubova917@gmail.com>
2025-05-14 15:18:56 +0500

    masala

---
+++ script.js
@@ -0,0 +1,25 @@
+// 1-masala
+// let a =+prompt("sonni kiriting")
+// if(a===1){
+//     console.log(6);
+// }else if(a===2){
+//     console.log(5);
+// }else if(a===3){
+//     console.log(4);
+// }else if(a===4){
+//     console.log(3);
+// }else if(a===5){
+//     console.log(2);
+// }else if(a===6){
+//     console.log(1);
+// }
+// 2-masala
+let i = +prompt("sonni kiriting");
+if (i / 3 === 0) {
+  console.log("Fizzy");
+} else if (i / 5 === 0) {
+  console.log("Bizzy");
+} else if (i / 3 === 0 && i / 5 === 0) {
+  console.log("FizzyBizzy");
+}
+
